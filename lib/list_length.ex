568afb94059bb20da20e2fed06df825c25d42897
Renan Teixeira <git@renanteixeira.com.br>
2021-07-01 23:33:57 -0300

    Function List Length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    # TO DO
+  def call(list), do: len(list, 0)
+
+  defp len([], acc), do: acc
+
+  defp len([_head | _tail] = list, _acc) do
+    Enum.count(list)
   end
 end
